e3ab2851899adadb9c62351ab27d4ef2e110a344
oscargfloresb <oscargfloresb@outlook.com>
2026-06-03 23:51:23 -0500

    Update

--- customize-debian.sh
+++ customize-debian.sh
@@ -21,16 +21,12 @@ apt update && apt upgrade -y && apt install -y \
     c3270 \
     gimp \
     vlc \
-    pspp \
-    loupe \
-    papers \
     openjdk-21-jdk \
     python3 \
     python3-pip \
     python3-venv \
     python3-tk \
     plymouth-themes \
-    gnome-software \
     gnome-text-editor \
     gnome-console \
     gnome-calculator \
